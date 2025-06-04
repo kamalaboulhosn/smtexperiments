ad0c4c299f29b6d7e07fca5aafc6186bef5c16ae
Kamal Aboul-Hosn <kamal.aboulhosn@gmail.com>
2025-06-04 11:30:59 -0400

    Move verify to verifyId

--- udfs/verifyId.js
+++ udfs/verifyId.js
@@ -1,4 +1,4 @@
-function validateAmount(message, metadata) {
+function verifyId(message, metadata) {
   const data = JSON.parse(message.data);
   if (data["id"] < 0) {
     throw new Error("ID is invalid");
