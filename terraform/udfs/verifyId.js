7ee24bc10552792d8d0100e5b28c22881ccbe2eb
Kamal Aboul-Hosn <kamal.aboulhosn@gmail.com>
2025-06-10 19:08:37 -0400

    Revert to old UDF

--- terraform/udfs/verifyId.js
+++ terraform/udfs/verifyId.js
@@ -1,7 +1,6 @@
 function verifyId(message, metadata) {
   const data = JSON.parse(message.data);
-  const id = data["id"];
-  if (!Number.isInteger(id) || id < 0) {
+  if (data["id"] < 0) {
     throw new Error("ID is invalid");
   }
   return message;
